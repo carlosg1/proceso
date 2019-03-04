cb62278e7d994bf9f5c419dd92876a7cb5179995
Carlos Garcia <carlosgctes@gmail.com>
2019-03-04 16:48:57 -0300

    hasta aca ya lee los registros de la base de datos

--- calle/index.php
+++ calle/index.php
@@ -4,7 +4,22 @@
   * Developer: Carlos Garcia > carlosgctes@gmail
   */
 
-  
+require('../conexion.php');
+
+$qry_vw_calles26_02 = "select * from \"gismcc\".\"vw_calles26-02\" order by id_calles limit 15";
+
+$rst_vw_calles26_02 = $conPdoPg->query($qry_vw_calles26_02);
+
+/***** imprime los resultados  *****/
+echo '"id_calles";"the_geom"' . "<br />";
+
+while($registro = $rst_vw_calles26_02->fetchObject()){
+
+    echo $registro->id_calles, ';', $registro->the_geom_calles, '<br />';
 
 
+}
+
+$rst_vw_calles26_02 = null;
+$conPdoPg = null;
 ?>
