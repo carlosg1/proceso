010071b6b107a0002ba1bc7a951eb23002f3d0b1
Carlos Garcia <carlosgctes@gmail.com>
2019-03-04 22:24:13 -0300

    Agregue la funcion que escribe en un archivo log

--- calle/index.php
+++ calle/index.php
@@ -16,24 +16,39 @@
  /* 
   * proceso de control de la tabla de calles
   * Developer: Carlos Garcia > carlosgctes@gmail
-  / fecha: 05/03/2019
+  * fecha: 05/03/2019
+  *
   */
+
+header("Content-Type: text/html");
+header("charset: utf-8");
+
+@error_reporting(E_ALL);
 
 require('../conexion.php');
 
-$qry_vw_calles26_02 = "select * from \"gismcc\".\"vw_calles26-02\" order by id_calles limit 15";
+$qry_vw_calles26_02 = "select * from \"gismcc\".\"vw_calles26-02\" order by id_calles limit 20";
 
 $rst_vw_calles26_02 = $conPdoPg->query($qry_vw_calles26_02);
 
+$file = fopen("AnalisisCalle.log", "w");
+
+fwrite($file, "\n\nInicio Analisis " . date('d/m/Y H:i:s') . PHP_EOL);
+
 /***** imprime los resultados  *****/
-echo '"id_calles";"the_geom"' . "<br />";
+echo 'id_calles | Observacion' . "<br />";
+
+fwrite($file, 'id_calles | Observacion' . PHP_EOL);
 
 while($registro = $rst_vw_calles26_02->fetchObject()){
 
-    echo $registro->id_calles, ';', $registro->the_geom_calles, '<br />';
+    echo $registro->id_calles . ' => ' . "Se recorrio el registro como prueba", '<br />';
 
+    fwrite($file, $registro->id_calles . ' => ' . "Se recorrio el registro como prueba" . PHP_EOL);
 
 }
+
+fclose($file);
 
 $rst_vw_calles26_02 = null;
 $conPdoPg = null;
