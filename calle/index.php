f9bb8f1d1efb8fdf490e9cc1e0845e45836c3f88
Carlos Garcia <carlosgctes@gmail.com>
2019-03-06 11:29:51 -0300

    Agregue varios controles comentados que hay que desarrollar

--- calle/index.php
+++ calle/index.php
@@ -23,11 +23,30 @@
 header("Content-Type: text/html");
 header("charset: utf-8");
 
+/* validacion de ususario */
+/*
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="Mi dominio"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo '<h1>Acceso cancelado por el usuario</h1>';
+    exit;
+} else {
+    echo "<p>Hola {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Introdujo {$_SERVER['PHP_AUTH_PW']} como su contraseña.</p>";
+}
+*/
+
 @error_reporting(E_ALL);
 
 require('../conexion.php');
 
-$qry_vw_calles26_02 = "select * from \"gismcc\".\"vw_calles26-02\" order by id_calles limit 20";
+// verifico si hay id_calles duplicados
+
+// verifico si hay id_calles con valor cero
+
+// verifico si hay id_calles en null
+
+$qry_vw_calles26_02 = "select * from \"gismcc\".\"vw_calles26-02\" where id_calles = 1638 order by id_calles limit 2";
 
 $rst_vw_calles26_02 = $conPdoPg->query($qry_vw_calles26_02);
 
@@ -40,16 +59,25 @@ echo 'id_calles | Observacion' . "<br />";
 
 fwrite($file, 'id_calles | Observacion' . PHP_EOL);
 
+/* preparo la consulta para consultar la tabla calles */
+$qry_calles = 'select * from gismcc.calles where id_calles = :p1';
+
+$stm_calles = $conPdoPg->prepare($qry_calles, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+
+
 while($registro = $rst_vw_calles26_02->fetchObject()){
 
-    echo $registro->id_calles . ' => ' . "Se recorrio el registro como prueba", '<br />';
+    $stm_calles->execute(array(':p1' => $registro->id_calles));
 
-    fwrite($file, $registro->id_calles . ' => ' . "Se recorrio el registro como prueba" . PHP_EOL);
+    $resultado = $stm_calles->fetchAll();
+
+    var_dump( $resultado );
 
 }
 
 fclose($file);
 
+$stm_calles = null;
 $rst_vw_calles26_02 = null;
 $conPdoPg = null;
-?>
+
